e9cee08f5958afaab3ee2d11ec357f90ee7e487d
Valerie Ricot <valeriericot@gmail.com>
2021-02-09 15:08:03 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"))
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', { cats })
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.render('subreddit', { subreddit });
+})
+
+app.get('/random', (req, res) => {
+    const num = Math.floor(Math.random() * 10) +1;
+    res.render('random', {num})
+}) 
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000!")
+})
